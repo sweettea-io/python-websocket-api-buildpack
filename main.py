be64c5106c6113c89c5c5215835704feaec50248
Ben Whittle <benwhittle31@gmail.com>
2017-11-27 01:35:55 -0500

    removing logging -- forgot Docker messes with timing

--- main.py
+++ main.py
@@ -84,17 +84,14 @@ def perform(team=None, team_uid=None, prediction=None, prediction_uid=None, s3_b
   # be renamed to <prediction_uid>/ to avoid conflicts with user's project files
 
   # Get refs to the modules inside our src directory
-  print('Importing modules...')
   model_fetcher = get_src_mod(prediction_uid, 'model_fetcher')
   definitions = get_src_mod(prediction_uid, 'definitions')
 
   # Read the config file in the project
-  print('Validating config...')
   config_path = getattr(definitions, 'config_path')
   config = read_config(config_path)
 
   # Get ref to the exported predict method
-  print('Getting ref to predict method...')
   predict_method = get_predict_method(config)
 
   # Fetch the latest model from S3
@@ -108,13 +105,11 @@ def perform(team=None, team_uid=None, prediction=None, prediction_uid=None, s3_b
 
   save_path = os.path.abspath(os.path.join(os.path.dirname(__file__), model_path))
 
-  print('Fetching latest model...')
   model_fetcher.fetch(cloud_path=cloud_path,
                       bucket=s3_bucket_name,
                       save_to=save_path)
 
   # Create API
-  print('Configuring Flask API...')
   app = Flask(__name__)
   api = Api(app=app, version='0.0.1', title='{} API'.format(prediction))
   namespace = api.namespace('api')
