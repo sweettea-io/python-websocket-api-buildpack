336a59d0dd4a6043eb32746bd4dfc7d5c237a338
Ben Whittle <benwhittle31@gmail.com>
2017-12-24 00:30:08 -0500

    adding PUT /model route; refactoring code flow

--- main.py
+++ main.py
@@ -5,29 +5,26 @@ from flask import Flask, request
 from flask_restplus import Api, Resource
 
 
-def get_envs():
-  # {key: <is_param>}
-  required_envs = {
-    'AWS_ACCESS_KEY_ID': False,
-    'AWS_SECRET_ACCESS_KEY': False,
-    'AWS_REGION_NAME': False,
-    'S3_BUCKET_NAME': True,
-    'DATASET_DB_URL': False,
-    'DATASET_TABLE_NAME': False,
-    'PREDICTION': True,
-    'PREDICTION_UID': False,
-    'CLIENT_ID': False,
-    'CLIENT_SECRET': False
-  }
+def validate_envs():
+  required_envs = [
+    'AWS_ACCESS_KEY_ID',
+    'AWS_SECRET_ACCESS_KEY',
+    'AWS_REGION_NAME',
+    'S3_BUCKET_NAME',
+    'DATASET_DB_URL',
+    'DATASET_TABLE_NAME',
+    'PREDICTION',
+    'PREDICTION_UID',
+    'CLIENT_ID',
+    'CLIENT_SECRET',
+    'INTERNAL_MSG_TOKEN'
+  ]
 
   missing = [k for k in required_envs if k not in os.environ]
 
   if missing:
     print('Not starting API. The following env vars not provided: {}'.format(', '.join(missing)))
     exit(1)
-
-  # Only return the envs that we need as params
-  return {k.lower(): os.environ.get(k) for k in required_envs if required_envs[k]}
 
 
 def read_config(config_path):
@@ -41,141 +38,172 @@ def read_config(config_path):
   return config
 
 
-def get_predict_method(config):
-  predict_mod_str, predict_func_str = config.get('predict').split(':')
+def get_exported_method(config, key=None):
+  module_str, function_str = config.get(key).split(':')
 
-  if not predict_mod_str:
-    print('No module specified for predicting. Only the function was specified.')
+  if not module_str:
+    print('No module specified for config method({}={}).'.format(key, config.get(key)))
     exit(1)
 
-  predict_mod = importlib.import_module(predict_mod_str)
+  module = importlib.import_module(module_str)
 
-  if not predict_mod:
-    print('No module to import at destination: {}'.format(predict_mod_str))
+  if not module:
+    print('No module to import at destination: {}'.format(module_str))
     exit(1)
 
-  if not hasattr(predict_mod, predict_func_str):
-    print('No function named {} exists on module {}'.format(predict_func_str, predict_mod_str))
+  if not hasattr(module, function_str):
+    print('No function named {} exists on module {}'.format(function_str, module_str))
     exit(1)
 
-  return getattr(predict_mod, predict_func_str)
+  return getattr(module, function_str)
 
 
 def get_src_mod(name):
   return importlib.import_module('{}.{}'.format(os.environ.get('PREDICTION_UID'), name))
 
 
-def perform(prediction=None, s3_bucket_name=None):
-  # Get refs to the modules inside our src directory
-  model_fetcher = get_src_mod('model_fetcher')
-  definitions = get_src_mod('definitions')
-
-  # Read the config file in the project
-  config_path = getattr(definitions, 'config_path')
-  config = read_config(config_path)
-
-  # Get ref to the exported predict method
-  predict_method = get_predict_method(config)
-
-  # Fetch the latest model from S3
-  model_path = config.get('model')
-
-  if '.' in model_path:
-    model_file_type = model_path.split('.').pop()
-    cloud_path = prediction + '.' + model_file_type
-  else:
-    cloud_path = prediction
-
-  save_path = os.path.abspath(os.path.join(os.path.dirname(__file__), model_path))
-
-  model_fetcher.fetch(cloud_path=cloud_path,
-                      bucket=s3_bucket_name,
-                      save_to=save_path)
-
-  return predict_method
-
-
-def client_valid(body, headers):
+def validate_client(body, headers):
   return body.get('client_id') == os.environ.get('CLIENT_ID') and \
          headers.get('TensorCI-Api-Secret') == os.environ.get('CLIENT_SECRET')
 
 
-params = get_envs()
-predict = perform(**params)
+def validate_core(headers):
+  return headers.get('TensorCI-Internal-Msg-Token') != os.environ.get('INTERNAL_MSG_TOKEN')
 
+
+def define_routes(api, namespace, 
+               predict_method=None, reload_model_method=None, 
+               model_fetcher=None, model_path=None):
+
+  # Get predictions
+  @namespace.route('/predict')
+  class Prediction(Resource):
+
+    def post(self):
+      try:
+        payload = api.payload or {}
+      except BaseException:
+        payload = {}
+
+      if not validate_client(payload, request.headers):
+        return {'ok': False, 'error': 'unauthorized'}, 401
+
+      try:
+        result = predict_method(payload)
+      except BaseException as e:
+        print('Error making prediction: {}'.format(e))
+        return {'ok': False, 'error': 'prediction_error'}, 500
+
+      return {'ok': True, 'prediction': result}, 200
+
+  
+  # Update the dataset
+  @namespace.route('/dataset')
+  class Dataset(Resource):
+
+    def put(self):
+      try:
+        payload = api.payload or {}
+      except BaseException:
+        payload = {}
+
+      if not validate_client(payload, request.headers):
+        return {'ok': False, 'error': 'unauthorized'}, 401
+
+      # Ensure new records were provided
+      records = payload.get('records') or []
+
+      if not records:
+        return {'ok': False, 'error': 'no_records_provided'}, 500
+
+      # Get the JSON data schema for the dataset table
+      table_name = os.environ.get('DATASET_TABLE_NAME')
+      dataset_schema = dataset_db.data_schema(table_name)
+
+      if not dataset_schema:
+        return {'ok': False, 'error': 'empty_dataset'}
+
+      # Validate the schema of the provided records matches that of the dataset
+      for r in records:
+        rset = set(r)
+
+        if rset != dataset_schema:
+          print('Invalid data schema when updating dataset.\nActual: {}\nExpected: {}'.format(rset, dataset_schema))
+          return {'ok': False, 'error': 'invalid_data_schema'}, 500
+
+      # Insert the new records
+      dataset_db.populate_records(records, table=table_name)
+
+      return {'ok': True, 'message': 'Successfully updated dataset'}, 200
+
+  if reload_model_method:
+    # Notification to fetch the latest model
+    @namespace.route('/model')
+    class Model(Resource):
+  
+      def put(self):
+        if not validate_core(request.headers):
+          return {'ok': False, 'error': 'forbidden'}, 403
+  
+        try:
+          # Update model file with latest from S3
+          model_fetcher.download_model(model_path)
+        except BaseException as e:
+          print('Error fetching latest model with error, {}.'.format(e))
+          return {'ok': False, 'error': 'unknown_error'}
+  
+        # Reload model into project
+        try:
+          reload_model_method()
+        except BaseException as e:
+          print('Error reloading model with error, {}.'.format(e))
+          return {'ok': False, 'error': 'reload_model_error'}
+  
+        return {'ok': True}, 200
+
+
+# Validate required env vars
+validate_envs()
+
+# Get exported src modules
+dataset_db = get_src_mod('dataset_db')
+definitions = get_src_mod('definitions')
+model_fetcher = get_src_mod('model_fetcher')
+
+# Read the project's config file
+config = read_config(definitions.config_path)
+
+# Get exported config file info ('predict', 'reload_model', 'model')
+predict = get_exported_method(config, key='predict')
+reload_model = None
+
+if 'reload_model' in config:
+  reload_model = get_exported_method(config, key='reload_model')
+
+model_path = config.get('model')
+
+# Fetch the latest model file from S3
+model_fetcher.download_model(model_path)
+
+# Create Flask app and API
 app = Flask(__name__)
 api = Api(version='0.0.1', title='API')
 namespace = api.namespace('api')
 
-dataset_db = get_src_mod('dataset_db')
+# Define the API's routes
+define_routes(api, namespace,
+              predict_method=predict,
+              reload_model_method=reload_model,
+              model_fetcher=model_fetcher,
+              model_path=model_path)
 
-
-@namespace.route('/predict')
-class Predict(Resource):
-  def post(self):
-    # Get payload
-    try:
-      payload = api.payload or {}
-    except BaseException:
-      payload = {}
-
-    # Ensure valid client id and secret
-    if not client_valid(payload, request.headers):
-      return {'ok': False, 'error': 'unauthorized'}, 401
-
-    # Get prediction
-    try:
-      prediction = predict(payload)
-    except BaseException as e:
-      print('Error making prediction: {}'.format(e))
-      return {'ok': False, 'error': 'prediction_error'}, 500
-
-    return prediction, 200
-
-
-@namespace.route('/dataset')
-class Predict(Resource):
-  def put(self):
-    try:
-      # Parse payload
-      payload = api.payload or {}
-    except BaseException:
-      payload = {}
-
-    # Ensure valid client id and secret
-    if not client_valid(payload, request.headers):
-      return {'ok': False, 'error': 'unauthorized'}, 401
-
-    # Ensure new records were even provided
-    records = payload.get('records') or []
-
-    if not records:
-      return {'ok': False, 'error': 'no_records_provided'}, 500
-
-    # Get the JSON data schema for the dataset table
-    table_name = os.environ.get('DATASET_TABLE_NAME')
-    dataset_schema = dataset_db.data_schema(table_name)
-
-    if not dataset_schema:
-      return {'ok': False, 'error': 'empty_dataset'}
-
-    # Validate the schema of the provided records matches that of the dataset
-    for r in records:
-      rset = set(r)
-
-      if rset != dataset_schema:
-        print('Invalid data schema when updating dataset.\nActual: {}\nExpected: {}'.format(rset, dataset_schema))
-        return {'ok': False, 'error': 'invalid_data_schema'}, 500
-
-    # Insert the new records
-    dataset_db.populate_records(records, table=table_name)
-
-    return {'ok': True, 'message': 'Successfully updated dataset'}, 200
-
-
+# Attach Flask app to the API
 api.init_app(app)
 
+# app will be imported by uwsgi on prod
 
+
+# Run built-in Flask server on dev
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 80))
   app.run(host='0.0.0.0', port=port, debug=True)
--- src/model_fetcher.py
+++ src/model_fetcher.py
@@ -1,6 +1,36 @@
 import os
 import boto3
+import shutil
 from boto3.s3.transfer import TransferConfig, S3Transfer
+
+
+def download_model(model_path):
+  # Construct cloud model path by combining prediction slug
+  # with file extension used in provided model_path.
+  prediction_slug = os.environ.get('PREDICTION')
+  cloud_path = prediction_slug
+  tmp_path = 'tmp/model-{}'.format(prediction_slug)
+
+  if '.' in model_path:
+    ext = model_path.split('.').pop()
+    cloud_path += '.{}'.format(ext)
+    tmp_path += '.{}'.format(ext)
+
+  # Construct absolute paths from relative ones
+  abs_tmp_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', tmp_path))
+  abs_model_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', model_path))
+
+  # Ensure all directories exist in each of our absolute paths
+  makedirs(abs_tmp_path)
+  makedirs(abs_model_path)
+
+  # Download model file from S3 to abs_tmp_path
+  fetch(cloud_path=cloud_path,
+        bucket=os.environ.get('S3_BUCKET_NAME'),
+        save_to=abs_tmp_path)
+
+  # Replace the active model file with the new one
+  shutil.move(abs_tmp_path, abs_model_path)
 
 
 def fetch(cloud_path=None, bucket=None, save_to=None):
@@ -8,3 +38,10 @@ def fetch(cloud_path=None, bucket=None, save_to=None):
   config = TransferConfig(use_threads=False)
   transfer = S3Transfer(client, config)
   transfer.download_file(bucket, cloud_path, save_to)
+
+
+def makedirs(filepath):
+  comps = filepath.split('/')
+  comps.pop()
+  dir = '/'.join(comps)
+  os.makedirs(dir)
